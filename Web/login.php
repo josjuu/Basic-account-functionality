ebb99808e50b93fbc74d9efd7486b63e52d21a23
Jos Mutter <jos.mutter@hotmail.com>
2018-10-07 12:07:05 +0200

    Webpages added.

---
+++ Web/login.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+    <link rel="stylesheet" href="../Assets/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+    <div class="row ">
+        <div class="col-md-5 mx-auto">
+            <br>
+            <div class="card">
+                <div class="card-header">Login</div>
+                <div class="card-body">
+                    <form>
+                        <div class="form-group">
+                            <label for="login-email">Email address</label>
+                            <input type="email" class="form-control" id="login-email" placeholder="Enter email">
+                        </div>
+                        <div class="form-group">
+                            <label for="login-password">Password</label>
+                            <input type="password" class="form-control" id="login-password" placeholder="Password">
+                        </div>
+                        <button id="login-submit" type="button" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+<script src="../Assets/js/bootstrap.min.js"></script>
+<script src="../Assets/js/Account.js"></script>
+</body>
+</html>
